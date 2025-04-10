a251b689ac4258dac62ec2d07ae8f3882636b952
solver-mathanraj <matheesh1214@gmail.com>
2025-04-10 19:01:31 +0530

    fix: env file added

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+require("dotenv").config(); 
 const { v4: uuidv4 } = require("uuid");
 
 const app = express();
